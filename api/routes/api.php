97558651541ac69b2365467bc1041e3d952e6c6d
steveislg@gmail.com <Andres2805+>
2022-07-21 11:32:59 -0400

    Creacion de Rutas del Backend

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\TaskController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+////Rutas Para el controlador de Tareas
+Route::controller(TaskController::class)->group(function (){
+    Route::get('/tasks','index');////Ruta para el listado de tareas
+    Route::post('/task','store');////Ruta para registrar una tarea
+    Route::get('/task/{id}','show');////Ruta para mostrar una tarea
+    Route::put('/task/{id}','update');////Ruta para actualizar una tarea
+    Route::delete('/task/{id}','destroy');////Ruta para eliminar una tarea
+
 });
