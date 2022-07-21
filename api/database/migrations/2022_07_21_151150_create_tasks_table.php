20e0f980e8d36fbdea6e85f4711af5ed2a845ca1
steveislg@gmail.com <Andres2805+>
2022-07-21 11:21:05 -0400

    Actualizacion Migracion de la Tabla de Tareas

--- api/database/migrations/2022_07_21_151150_create_tasks_table.php
+++ api/database/migrations/2022_07_21_151150_create_tasks_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+////Migracion para tabla de Tareas////
 class CreateTasksTable extends Migration
 {
     /**
@@ -15,6 +15,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->text('description');
+            $table->string('name_response');
+            $table->string('last_name_response');
             $table->timestamps();
         });
     }
