aa7dcd36be2b82bf63018555229cd56cf612747a
steveislg@gmail.com <Andres2805+>
2022-07-21 12:03:10 -0400

    Actualizacion Index  App js / Creacion Componente

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShowTask from './components/ShowTask';
+import CreateTask from './components/CreateTask';
+import EditTask from './components/EditTask';
+
 
 function App() {
-  return (
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ShowTask/>}/>
+          <Route path='/create' element={<CreateTask/>}/>
+          <Route path='/edit/:id' element={<EditTask/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
