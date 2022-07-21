63ec7c51aff4a7a1f7ba52c9b1a9a1798d115a7b
steveislg@gmail.com <Andres2805+>
2022-07-21 12:16:53 -0400

    Creacion componente para editar tarea

---
+++ reactfront/src/components/EditTask.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import React, { useState, useEffect} from "react";
+import { useNavigate, useParams  } from "react-router-dom";
+
+const EditTask =() =>{
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [name_response, setNameResponse] = useState('')
+    const [last_name_response, setLastNameResponse] = useState('')
+    const navigate = useNavigate()
+
+    const {id} = useParams()
+
+    const update = async (e) =>{
+        e.preventDefault()
+        console.log(title)
+        console.log(description)
+        console.log(name_response)
+        console.log(last_name_response)
+        await axios.put('http://localhost:8000/api/task/${id}',{
+            title:title,
+            description:description,
+            name_response:name_response,
+            last_name_response:last_name_response
+        })
+         navigate('/')
+     }
+
+    useEffect(()=>{
+        const getTaskById =  async ()=>{
+            const response =  await axios.get('http://localhost:8000/api/task/${id}')
+            console.log(response.data)
+            setTitle( response.data.title )
+            setDescription( response.data.description )
+            setNameResponse( response.data.name_response )
+            setLastNameResponse( response.data.last_name_response)
+
+        }
+
+        getTaskById()
+
+    },[])
+
+
+    return(
+        <div>
+        <h3><b>Editar Tarea</b></h3>
+        <form onSubmit={update}>
+            <div className='mb-3'>
+                <label className='form-label'><b>Titulo: </b></label>
+                <input value={title} onChange={(e)=>setTitle(e.target.value)} type='text' className='form-control'/>
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'><b>Descripcion: </b></label>
+                <input value={description} onChange={(e)=>setDescription(e.target.value)} type='text' className='form-control'/>
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'><b>Nombre Responsable</b></label>
+                <input value={name_response} onChange={(e)=>setNameResponse(e.target.value)} type='text' className='form-control'/>
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'><b>Apellido Responsable</b></label>
+                <input value={last_name_response} onChange={(e)=>setLastNameResponse(e.target.value)} type='text' className='form-control'/>
+            </div>
+            <button type='submit' className='btn btn-primary'>Editar</button>
+        </form>
+        
+    </div>
+    )
+
+}
+
+export default EditTask
